5ada71f04b2f0c919922206e0aeb753e771bb44c
Ahmadheysami <alheythami@gmail.com>
2023-09-18 08:06:11 -0700

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+let modelViewer = document.querySelector("model-viewer");
+
+(() => {
+  const orbitCycle = [
+    "45deg 55deg 4m",
+    "-60deg 110deg 2m",
+    "-110deg 0deg 2m",
+    "110deg 55deg 2m",
+    modelViewer.cameraOrbit,
+  ];
+
+  setInterval(() => {
+    const currentOrbitIndex = orbitCycle.indexOf(modelViewer.cameraOrbit);
+    modelViewer.cameraOrbit =
+      orbitCycle[(currentOrbitIndex + 1) % orbitCycle.length];
+  }, 3000);
+})();
